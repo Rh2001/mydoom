6b2b4271d9126212b61ddbeac7f26fd643dd6874
Roham H <100292931+Rh2001@users.noreply.github.com>
2026-03-14 00:39:37 +0200

    Fix shader path

--- src/main.cpp
+++ src/main.cpp
@@ -10,9 +10,12 @@ int main()
 {
 
     float vertices[] = {
-        -0.5f,-0.5f,0.0f,  0.5f,-0.5f,0.0f,
-         0.5f, 0.5f,0.0f, -0.5f, 0.5f,0.0f
-    };
+    //   position        // color
+    -0.5f,-0.5f,0.0f,   1.0f,0.0f,0.0f,
+     0.5f,-0.5f,0.0f,   0.0f,1.0f,0.0f,
+     0.5f, 0.5f,0.0f,   0.0f,0.0f,1.0f,
+    -0.5f, 0.5f,0.0f,   1.0f,1.0f,1.0f
+};
 
     unsigned int indices[] = {
         0,1,2,
@@ -39,7 +42,7 @@ int main()
         return -1;
     }
 
-    Shader shader("shaders/vertex.glsl", "shaders/fragment.glsl"); // Load and compile shaders (after GL is loaded)
+    Shader shader("../shaders/vertex.glsl", "../shaders/fragment.glsl"); // Load and compile shaders (after GL is loaded)
 
     unsigned int VAO,VBO,EBO;
     glGenVertexArrays(1,&VAO);
@@ -51,8 +54,10 @@ int main()
     glBufferData(GL_ARRAY_BUFFER,sizeof(vertices),vertices,GL_STATIC_DRAW);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER,sizeof(indices),indices,GL_STATIC_DRAW);
-    glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,3*sizeof(float),(void*)0);
+    glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,6*sizeof(float),(void*)0);
     glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1,3,GL_FLOAT,GL_FALSE,6*sizeof(float),(void*)(3*sizeof(float)));
+    glEnableVertexAttribArray(1);
     glBindVertexArray(0);
 
     while (!glfwWindowShouldClose(window))
